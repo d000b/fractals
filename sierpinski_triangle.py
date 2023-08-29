09b01ddc8703ad3976df7dd0373902b6e9e2116d
klavssprugevics <klavs.sprugevics@gmail.com>
2023-08-29 21:02:12 +0300

    Update

--- sierpinski_triangle.py
+++ sierpinski_triangle.py
@@ -18,12 +18,20 @@ def sierpinski_triangle(iterations, length, startPos, ax, midConstant, color):
     if iterations <= 0:
         return
     iterations = iterations - 1
+
+    # ax.plot(startPos[0], startPos[1], marker='o', color='green', markersize=8)
+    ax.plot(startPos[0] + midConstant / 4, startPos[1] + midConstant / 2, marker='o', color='purple', markersize=8)
+
+    ax.add_patch(draw_triangle_down(length / 2, midpoint([0, 0], [startPos[0], startPos[1]]), color))
+    ax.add_patch(draw_triangle_down(length / 2, midpoint([startPos[0], startPos[1]], [startPos[0] + length, startPos[1] + length]), "pink"))
     
-    ax.add_patch(draw_triangle_down(length / 2, [startPos[0], startPos[1]], color))
+    # ax.add_patch(draw_triangle_down(length / 2, [startPos[0], startPos[1]], color))
+    # ax.add_patch(draw_triangle_down(length / 2, [startPos[0] + length * 2, startPos[1]], "green"))
+
     # ax.add_patch(draw_triangle_down(length / 2, midpoint(startPos, [length + length / 2, midConstant + midConstant / 2]), 'white'))
     # ax.add_patch(draw_triangle_down(length / 2, midpoint(startPos, [length * 2 + length / 2, midConstant / 2]), 'white'))
 
-    sierpinski_triangle(iterations, length / 2, midpoint([0, 0], [length / 4, startPos[1]]), ax, midConstant, 'yellow')
+    sierpinski_triangle(iterations, length / 2, midpoint([0, 0], [startPos[0], startPos[1]]), ax, midConstant, 'yellow')
     # sierpinski_triangle(iterations, length / 2, midpoint([startPos[0], startPos[1]], [length / 4, startPos[1]]), ax, midConstant, 'yellow')
 
 
@@ -35,15 +43,10 @@ ax.set_aspect('equal')
 midConstant = 0.8660254
 ax.add_patch(draw_triangle_up(1, [0, 0], 'blue'))
 first_c = midpoint([0, 0], [0.5, midConstant])
+
 ax.add_patch(draw_triangle_down(0.5, first_c, 'white'))
 
-sierpinski_triangle(100, 0.5, midpoint([0, 0], first_c), ax, midConstant, 'yellow')
+sierpinski_triangle(4, 0.5, first_c, ax, midConstant, 'yellow')
 
-# # 2nd iter.
-# ax.add_patch(draw_triangle_down(0.5 / 2, midpoint([0, 0], [0.5 / 2, 0.8660254 / 2]), 'white'))
-# ax.add_patch(draw_triangle_down(0.5 / 2, midpoint([0, 0], [0.5 + 0.5 / 2, 0.8660254 + 0.8660254 / 2]), 'white'))
-# ax.add_patch(draw_triangle_down(0.5 / 2, midpoint([0, 0], [0.5 * 2 + 0.25, 0.8660254 / 2]), 'white'))
-
-# ax.add_patch(draw_triangle_down(0.25, midpoint([0, 0], [0.25, 0.8660254 / 2]), 'white'))
 
 plt.show()
