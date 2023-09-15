b68db19c0d8dbcc5ccb08d8b3f5f941b002826f9
ksprugevics <klavs.dev@gmail.com>
2023-09-15 16:37:41 +0300

    Apollonian gasket - kissing circles

--- apollonian_gasket.py
+++ apollonian_gasket.py
@@ -1,60 +1,77 @@
-import math
 import matplotlib.pyplot as plt
+import numpy as np
+import math
 
 
 def initFigure():
     fig, ax = plt.subplots(figsize=(6, 6))
-    ax.set_xlim(-1, 1)
-    ax.set_ylim(-1, 1)
     ax.axis('off')
     ax.set_aspect('equal')
 
+    ax.set_xlim(-25, 25)
+    ax.set_ylim(-25, 25)
+
+
     fig.patch.set_facecolor('black')
-    ax.set_facecolor('black')
     return ax, fig
 
-def drawInitialCircles(ax, c0, c1, c2, r0, r1, r2):
-    outerCircle = plt.Circle(c0, r0, color='white', fill=False)
-    circle1 = plt.Circle(c1, r1, color='white', fill=False)
-    circle2 = plt.Circle(c2, r2, color='white', fill=False)
-    ax.add_patch(outerCircle)
-    ax.add_patch(circle1)
-    ax.add_patch(circle2)
 
 def drawCircle(ax, c, r):
     circle = plt.Circle(c, r, color='white', fill=False)
     ax.add_patch(circle)
 
-def descartesTheorem(a, b, c):
-    return a + b + c + 2 * (math.sqrt(a * b + b * c + c * a)),  a + b + c - 2 * (math.sqrt(a * b + b * c + c * a))
 
+def calculateThirdCircle(C1, C2, R1, R2, R3):
+    a = R1 + R2
+    b = R1 + R3
+    c = R3 + R2
+
+    area = calculateAreaHerons(a, b, c)
+    height = calculateHeightFromArea(area, a)
+    C = calculateThirdCenter(b, height, C1)
+    return C
+
+
+def calculateAreaHerons(a, b, c):
+    # semiperimeter
+    sp = (a + b + c) / 2
+    return math.sqrt(sp * (sp - a) * (sp - b) * (sp - c))
+
+
+def calculateHeightFromArea(area, base):
+    return 2 * area / base
+
+
+def calculateThirdCenter(hip, height, point):
+    segment = math.sqrt(hip ** 2 - height ** 2)
+    return (point[0] + segment, point[1] + height)
+
+
+def descartesTheorem(R1, R2, R3):
+    denominator = R1 * R2 + R2 * R3 + R3 * R1
+    common_term = 2 * math.sqrt(R1 * R2 * R3 * (R1 + R2 + R3))
+    
+    Xinterior = R1 * R2 * R3 / (denominator + common_term)
+    Xexterior = R1 * R2 * R3 / (denominator - common_term)
+    
+    return Xinterior, abs(Xexterior)
 
 
 if __name__ == "__main__":
     ax, fig = initFigure()
     
-    r0, r1, r2 = 1, 0.5, 0.5
-    c0, c1, c2 = (0, 0), (-r1, 0), (r2, 0)
-    drawInitialCircles(ax, c0, c1, c2, r0, r1, r2)
+    # Distance between A and B should make up to R1 + R2
+    # A, B = (-3, 0), (3, 0)
+    # R1, R2, R3 = 3, 3, 3
+    A, B = (-3, 0), (5, 0)
+    R1, R2, R3 = 3, 5, 3
 
-    # curvatures of each circle
-    cu0 = -1 / r0
-    cu1 = 1 / (r1)
-    cu2 = 1 / (r2)
-    # print(cu0, cu1, cu2)
-    cuN = descartesTheorem(cu0, cu1, cu2)
-    # print(cuN)
-    cuN = cuN[0]
-    rN = 1 / cuN
-    drawCircle(ax, (0, -r0 + rN), rN)
-    drawCircle(ax, (0, +r0 - rN), rN)
+    C = calculateThirdCircle(A, B, R1, R2, R3)
 
+    R4_i, R4_e = descartesTheorem(R1, R2, R3)
+    print(R4_i, R4_e)
 
-    cu4 = descartesTheorem(cuN, cu0, cu2)
-    # print(cu4)
-    cu4 = cu4[0]
-    r4 = 1 / cu4
-    drawCircle(ax, (0, -r0 + r4), r4)
-    drawCircle(ax, (0, +r0 - r4), r4)
-
+    drawCircle(ax, A, R1)
+    drawCircle(ax, B, R2)
+    drawCircle(ax, C, R3)
     plt.show()
