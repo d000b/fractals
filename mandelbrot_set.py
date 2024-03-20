aba10ff2c88a9f9094314de8c1e9744f1d0350cb
d000b <makc.hitman@gmail.com>
2024-03-20 10:19:52 +0800

    initial update

--- mandelbrot_set.py
+++ mandelbrot_set.py
@@ -1,17 +1,34 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
 
-def initFigure():
+class Context:
+    def __init__(self):
+        self.axis = None
+
+        self.screenspace = 1000, 1000  # Image resolution
+        self.x = -2.0, 1.0  # X-axis range
+        self.y = -1.5, 1.5  # Y-axis range
+        self.iterations = 100  # Maximum number of iterations
+
+
+def initFigure(x, y):
     fig, ax = plt.subplots(figsize=(6, 6))
-    ax.set_xlim(-2.0, 1)
-    ax.set_ylim(-1.5, 1.5)
+
+    x_min, x_max = x
+    y_min, y_max = y
+
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
+
     ax.axis('off')
     ax.set_aspect('equal')
 
     fig.patch.set_facecolor('black')
     ax.set_facecolor('black')
     return ax, fig
+
 
 def modifyColorMapping(n, max_iters):
     color = np.log(n + 1) / np.log(max_iters)
@@ -29,24 +46,82 @@ def isInMandelbrotSet(c, maxIters):
     return n
 
 
-if __name__ == "__main__":
-    width, height = 800, 800  # Image resolution
-    x_min, x_max = -2.0, 1.0    # X-axis range
-    y_min, y_max = -1.5, 1.5    # Y-axis range
-    max_iters = 1000  # Maximum number of iterations
+def maldebrot_impl(x, y, screenspace, iterations):
+    x_min, x_max = x
+    y_min, y_max = y
+    width, height = screenspace
 
-    mandelbrotSet = np.zeros((height, width))
+    re = np.linspace(x_min, x_max, width)
+    im = np.linspace(y_min, y_max, height)
 
-    real_vals = np.linspace(x_min, x_max, width)
-    imag_vals = np.linspace(y_min, y_max, height)
-
-    real, imag = np.meshgrid(real_vals, imag_vals)
+    real, imag = np.meshgrid(re, im)
     c = real + 1j * imag
 
-    mandelbrotSet = isInMandelbrotSet(c, max_iters)
+    mandelbrotSet = isInMandelbrotSet(c, iterations)
 
-    colorMap = modifyColorMapping(mandelbrotSet, max_iters)
+    return mandelbrotSet
 
-    initFigure()
-    plt.imshow(colorMap, extent=(x_min, x_max, y_min, y_max), cmap='inferno')
+
+def update(ctx: Context):
+    mandelbrotSet = maldebrot_impl(ctx.x, ctx.y, ctx.screenspace, ctx.iterations)
+
+    colorMap = modifyColorMapping(mandelbrotSet, ctx.iterations)
+
+    if ctx.axis is None:
+        axis, figure = initFigure(ctx.x, ctx.y)
+        ctx.axis = axis
+
+    plt.imshow(colorMap, extent=(*ctx.x, *ctx.y), cmap='inferno')
     plt.show()
+
+
+class Zoomable:
+    def __init__(self, plot, context: Context):
+        self.plot = plot
+        self.context = context
+
+        self.x = None
+        self.y = None
+
+        self.pressing = None
+        self.releasing = None
+        self.moving = None
+
+        self.connect()
+
+    def connect(self):
+        self.pressing = self.plot.connect('button_press_event', self.on_press)
+        self.releasing = self.plot.connect('button_release_event', self.on_release)
+        self.moving = self.plot.connect('motion_notify_event', self.on_motion)
+
+    def on_press(self, event):
+        self.x = event.xdata, None
+        self.y = event.ydata, None
+
+        print(self.x, self.y)
+
+    def on_motion(self, event):
+        pass
+
+    def on_release(self, event):
+        x, _ = self.x
+        y, _ = self.y
+
+        self.context.x = x, event.xdata
+        self.context.y = y, event.ydata
+
+        print(self.context.x, self.context.y)
+
+        update(self.context)
+
+    def disconnect(self):
+        self.plot.disconnect(self.pressing)
+        self.plot.disconnect(self.releasing)
+        self.plot.disconnect(self.moving)
+
+
+if __name__ == "__main__":
+    zoom = Zoomable(plot=plt, context=Context())
+
+    update(zoom.context)
+
